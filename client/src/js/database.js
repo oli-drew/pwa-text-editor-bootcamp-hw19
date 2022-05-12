b1754262e6e3607f3637f031e09bbba48dfe98f7
Oliver Drew <oli.drew92@gmail.com>
2022-05-12 16:27:56 +0100

    Actually fixed the error this time

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,9 @@ const initdb = async () =>
     },
   });
 
-// TODO:
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("PUT stuff in the database");
+  console.log("Saving to the database...");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
@@ -23,15 +22,13 @@ export const putDb = async (content) => {
   console.log("Text saved to the database");
 };
 
-// TODO:
 // Get all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
-  const result = await request;
-  if (result[0].content) return result[0].content;
+  const request = await store.get("content");
+  return request;
 };
 
 initdb();
